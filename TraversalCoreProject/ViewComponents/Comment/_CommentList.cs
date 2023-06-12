ca5fe5b2d862d9910a2570f3a6bb5646a20bfa70
senanurcoban <senacoban132@gmail.com>
2023-06-12 15:18:47 -0700

    comment & destination connect

---
+++ TraversalCoreProject/ViewComponents/Comment/_CommentList.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Comment
+{
+    public class _CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            
+            return View();
+        }
+    }
+}
