d92993abfa0f1a1a9ce5b52d2339b5d677c6902e
senanurcoban <senacoban132@gmail.com>
2023-06-13 21:39:01 -0700

    Identity Library added

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -16,6 +16,7 @@ namespace TraversalCoreProject.Controllers
         [HttpGet]
         public IActionResult DestinationDetails(int id)
         {
+            ViewBag.i = id;
             var values = destinationManager.TGetByID(id);
             return View(values);
         }
--- TraversalCoreProject/ViewComponents/Comment/_CommentList.cs
+++ TraversalCoreProject/ViewComponents/Comment/_CommentList.cs
@@ -1,14 +1,16 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.ViewComponents.Comment
 {
     public class _CommentList:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+        public IViewComponentResult Invoke(int id)
         {
-            
-            return View();
+            var values = commentManager.TGetDestinationById(id);
+            return View(values);
         }
     }
 }
