a1ef83c9f53e04653b16dd4109432e99462d56b7
senanurcoban <senacoban132@gmail.com>
2023-06-22 18:07:54 -0700

    Login & Profile pages updated

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -10,11 +10,12 @@ namespace TraversalCoreProject.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
-        public LoginController(UserManager<AppUser> userManager)
+        private readonly SignInManager<AppUser> _signInManager;
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
-            _userManager =userManager;
-
-		}
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
         [HttpGet]
         public IActionResult SignUp()
@@ -52,6 +53,23 @@ namespace TraversalCoreProject.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.username, p.password, false, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Profile", new { area = "Member" });
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
             return View();
         }
     }
